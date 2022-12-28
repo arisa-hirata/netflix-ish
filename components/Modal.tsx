fd5a361d04799183033499e3511546e9d098a894
Arisa Hirata <arisa.pirata@gmail.com>
2022-12-28 12:57:35 -0800

    Install Material UI packages and create modal component

--- components/Modal.tsx
+++ components/Modal.tsx
@@ -1,8 +1,19 @@
- function Modal() {
-  return (
-    <div>
+import MuiModal from '@mui/material/Modal'
+import { useRecoilState, useRecoilValue } from 'recoil'
+import { modalState } from '../atoms/modalAtom'
 
-    </div>
+function Modal() {
+  const [showModal, setShowModal] = useRecoilState(modalState)
+
+  const handleClose = () => {
+    setShowModal(false)
+  }
+  return (
+    <MuiModal open={showModal} onClose={handleClose}>
+      <>
+        Modal
+      </>
+    </MuiModal>
   )
 }
 

8b9dafa18d6395f76dac0cefe6829ce88262dd09
Arisa Hirata <arisa.pirata@gmail.com>
2022-12-29 12:45:50 -0800

    create Plan component and create stripe.ts file

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,6 +4,7 @@ import Header from '../components/Header'
 import Banner from '../components/Banner'
 import Row from '../components/Row'
 import Modal from '../components/Modal'
+import Plans from '../components/Plans'
 import requests from '../utils/requests'
 import { Movie } from '../types'
 import useAuth from '../hooks/useAuth'
@@ -35,8 +36,11 @@ const Home = ({
 }: Props) => {
   const { loading } = useAuth()
   const showModal = useRecoilValue(modalState)
+  const subscription = false
 
-  if (loading) return  null
+  if (loading || subscription === null) return null
+
+  if (!subscription) return <Plans />
 
   return (
     <div className={`relative h-screen bg-gradient-to-b from-gray-900/10 to-[#010511] lg:h-[140vh] ${
@@ -54,7 +58,6 @@ const Home = ({
           <Row title="Top Rated" movies={topRated} />
           <Row title="Action Thrillers" movies={actionMovies} />
           {/* My List Component*/}
-          {/* {list.length > 0 && <Row title="My List" movies={list} />} */}
 
           <Row title="Comedies" movies={comedyMovies} />
           <Row title="Scary Movies" movies={horrorMovies} />

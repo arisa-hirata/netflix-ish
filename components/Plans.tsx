0305e49ead10fb70c2d35847a2c1f677d4ad3842
Arisa Hirata <arisa.pirata@gmail.com>
2022-12-30 11:31:35 -0800

    Create Loader component and make functionally subscription function with Stripe test mode

--- components/Plans.tsx
+++ components/Plans.tsx
@@ -6,28 +6,37 @@ import { useState } from 'react'
 import useAuth from '../hooks/useAuth'
 import { loadCheckout } from '../lib/stripe'
 import Table from './Table'
-// import Loader from './Loader'
+import Loader from './Loader'
 
 interface Props {
   products: Product[]
-  selectedPlan: Product | null
 }
 
-function Plans({ products, selectedPlan }: Props) {
+function Plans({ products }: Props) {
   const { logout, user } = useAuth()
+  const [selectedPlan, setSelectedPlan] = useState<Product | null>(products[2])
+  const [isBillingLoading, setBillingLoading] = useState(false)
+
+  console.log(products)
+
+  const subscribeToPlan = () => {
+    if (!user) return
+
+    loadCheckout(selectedPlan?.prices[0].id!)
+    setBillingLoading(true)
+  }
 
   return (
     <div>
       <Head>
-        <title>Netflix-ish</title>
+        <title>Netflix</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-
       <header className="border-b border-white/10 bg-[#141414]">
         <Link href="/">
           <img
             src="https://rb.gy/ulxxee"
-            alt="Netflix-ish"
+            alt="Netflix"
             width={150}
             height={90}
             className="cursor-pointer object-contain"
@@ -40,6 +49,7 @@ function Plans({ products, selectedPlan }: Props) {
           Sign Out
         </button>
       </header>
+
       <main className="mx-auto max-w-5xl px-5 pt-28 pb-12 transition-all md:px-10">
         <h1 className="mb-3 text-3xl font-medium">
           Choose the plan that's right for you
@@ -67,6 +77,7 @@ function Plans({ products, selectedPlan }: Props) {
                   selectedPlan?.id === product.id ? 'opacity-100' : 'opacity-60'
                 }`}
                 key={product.id}
+                onClick={() => setSelectedPlan(product)}
               >
                 {product.name}
               </div>
@@ -74,6 +85,20 @@ function Plans({ products, selectedPlan }: Props) {
           </div>
 
           <Table products={products} selectedPlan={selectedPlan} />
+
+          <button
+            disabled={!selectedPlan || isBillingLoading}
+            className={`mx-auto w-11/12 rounded bg-[#E50914] py-4 text-xl shadow hover:bg-[#f6121d] md:w-[420px] ${
+              isBillingLoading && 'opacity-60'
+            }`}
+            onClick={subscribeToPlan}
+          >
+            {isBillingLoading ? (
+              <Loader color="dark:fill-gray-300" />
+            ) : (
+              'Subscribe'
+            )}
+          </button>
         </div>
       </main>
     </div>

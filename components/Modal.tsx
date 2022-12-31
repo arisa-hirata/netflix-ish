a7d259556b7cb9722677e2e9db3231091aaac002
Arisa Hirata <arisa.pirata@gmail.com>
2022-12-31 14:32:14 -0800

    Create userList custom hooks and add myList button to store myList data in firestore

--- components/Modal.tsx
+++ components/Modal.tsx
@@ -1,8 +1,6 @@
-import MuiModal from '@mui/material/Modal'
 import { useEffect, useState } from 'react'
 import { useRecoilState } from 'recoil'
 import { modalState, movieState } from '../atoms/modalAtom'
-import { Element, Genre, Movie } from '../types'
 import ReactPlayer from 'react-player/lazy'
 import { FaPlay } from 'react-icons/fa'
 import {
@@ -13,25 +11,51 @@ import {
   VolumeUpIcon,
   XIcon,
 } from '@heroicons/react/outline'
+import { Element, Genre, Movie } from '../types'
+import MuiModal from '@mui/material/Modal'
+import {
+  collection,
+  deleteDoc,
+  doc,
+  DocumentData,
+  onSnapshot,
+  setDoc,
+} from 'firebase/firestore'
+import { db } from '../firebase'
+import useAuth from '../hooks/useAuth'
+import toast, { Toaster } from 'react-hot-toast'
 
 function Modal() {
+  const [movie, setMovie] = useRecoilState(movieState)
   const [trailer, setTrailer] = useState('')
   const [showModal, setShowModal] = useRecoilState(modalState)
   const [muted, setMuted] = useState(true)
-  const [movie, setMovie] = useRecoilState(movieState)
-  const [data, setData] = useState()
   const [genres, setGenres] = useState<Genre[]>([])
+  const [addedToList, setAddedToList] = useState(false)
+  const { user } = useAuth()
+  const [movies, setMovies] = useState<DocumentData[] | Movie[]>([])
+
+  const toastStyle = {
+    background: 'white',
+    color: 'black',
+    fontWeight: 'bold',
+    fontSize: '16px',
+    padding: '15px',
+    borderRadius: '9999px',
+    maxWidth: '1000px',
+  }
 
   useEffect(() => {
     if (!movie) return
 
     async function fetchMovie() {
       const data = await fetch(
-        `https://api.themoviedb.org/3/${movie?.media_type === 'tv' ? 'tv' : 'movie'
-        }/${movie?.id}?api_key=${process.env.NEXT_PUBLIC_API_KEY
+        `https://api.themoviedb.org/3/${
+          movie?.media_type === 'tv' ? 'tv' : 'movie'
+        }/${movie?.id}?api_key=${
+          process.env.NEXT_PUBLIC_API_KEY
         }&language=en-US&append_to_response=videos`
       ).then((response) => response.json())
-
       if (data?.videos) {
         const index = data.videos.results.findIndex(
           (element: Element) => element.type === 'Trailer'
@@ -48,9 +72,61 @@ function Modal() {
 
   const handleClose = () => {
     setShowModal(false)
+    setMovie(null)
+    toast.dismiss()
   }
 
-  console.log(trailer);
+  // Find all the movies in the user's list
+  useEffect(() => {
+    if (user) {
+      return onSnapshot(
+        collection(db, 'customers', user.uid, 'myList'),
+        (snapshot) => setMovies(snapshot.docs)
+      )
+    }
+  }, [db, movie?.id])
+
+  // Check if the movie is already in the user's list
+  useEffect(
+    () =>
+      setAddedToList(
+        movies.findIndex((result) => result.data().id === movie?.id) !== -1
+      ),
+    [movies]
+  )
+
+  const handleList = async () => {
+    if (addedToList) {
+      await deleteDoc(
+        doc(db, 'customers', user!.uid, 'myList', movie?.id.toString()!)
+      )
+
+      toast(
+        `${movie?.title || movie?.original_name} has been removed from My List`,
+        {
+          duration: 8000,
+          style: toastStyle,
+        }
+      )
+    } else {
+      await setDoc(
+        doc(db, 'customers', user!.uid, 'myList', movie?.id.toString()!),
+        {
+          ...movie,
+        }
+      )
+
+      toast(
+        `${movie?.title || movie?.original_name} has been added to My List.`,
+        {
+          duration: 8000,
+          style: toastStyle,
+        }
+      )
+    }
+  }
+
+  console.log(addedToList)
 
   return (
     <MuiModal
@@ -59,8 +135,12 @@ function Modal() {
       className="fixed !top-7 left-0 right-0 z-50 mx-auto w-full max-w-5xl overflow-hidden overflow-y-scroll rounded-md scrollbar-hide"
     >
       <>
-        <button onClick={handleClose} className="modalButton absolute right-5 top-5 !z-40 h-9 w-9 border-none bg-[#181818] hover:bg-[#181818]">
-          <XIcon  className="h-6 w-6"/>
+        <Toaster position="bottom-center" />
+        <button
+          className="modalButton absolute right-5 top-5 !z-40 h-9 w-9 border-none bg-[#181818] hover:bg-[#181818]"
+          onClick={handleClose}
+        >
+          <XIcon className="h-6 w-6" />
         </button>
 
         <div className="relative pt-[56.25%]">
@@ -71,25 +151,29 @@ function Modal() {
             style={{ position: 'absolute', top: '0', left: '0' }}
             playing
             muted={muted}
-        />
+          />
           <div className="absolute bottom-10 flex w-full items-center justify-between px-10">
             <div className="flex space-x-2">
               <button className="flex items-center gap-x-2 rounded bg-white px-8 text-xl font-bold text-black transition hover:bg-[#e6e6e6]">
                 <FaPlay className="h-7 w-7 text-black" />
                 Play
               </button>
-              <button className="modalButton">
-                <PlusIcon className="h-7 w-10"/>
+              <button className="modalButton" onClick={handleList}>
+                {addedToList ? (
+                  <CheckIcon className="h-7 w-10" />
+                ) : (
+                  <PlusIcon className="h-7 w-10" />
+                )}
               </button>
               <button className="modalButton">
-                <ThumbUpIcon className="h-7 w-10"/>
+                <ThumbUpIcon className="h-6 w-10" />
               </button>
             </div>
-            <button onClick={() => setMuted(!muted)}>
+            <button className="modalButton" onClick={() => setMuted(!muted)}>
               {muted ? (
-                <VolumeOffIcon className="h-6 w-6"/>
+                <VolumeOffIcon className="h-6 w-10" />
               ) : (
-                <VolumeUpIcon className="h-6 w-6"/>
+                <VolumeUpIcon className="h-6 w-10" />
               )}
             </button>
           </div>
